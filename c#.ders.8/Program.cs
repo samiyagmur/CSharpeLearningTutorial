46a6cf19cfa8294c53fb3cfc2f572b409652d4ff
samiyagmur <77567437+samiyagmur@users.noreply.github.com>
2022-03-11 15:43:12 +0300

    master

--- c#.ders.8/Program.cs
+++ c#.ders.8/Program.cs
@@ -4,9 +4,36 @@ namespace c_.ders._8
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static async void Main(string[] args)
+        {   
+            //Ekrandan girilen sayıya kadar olan tek sayıları yazdır.
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            
+            for (int i = 0 ; i < sayac; i++)
+            {
+                if(i%2 == 1){
+                    Console.WriteLine(i);
+                }
+                
+            }
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for(int i = 1; i<1000;i++)
+            {
+                if (i%2 == 1)
+                {
+                    tekToplam = i + tekToplam;
+                }
+                else if (i%2 == 0)
+                {
+                    ciftToplam = i + ciftToplam;
+                }
+            }
+
+            Console.WriteLine("Tek sayıların toplamı: " + tekToplam);
+            Console.WriteLine("Çift sayıların toplamı: " + ciftToplam);
+
         }
     }
 }
