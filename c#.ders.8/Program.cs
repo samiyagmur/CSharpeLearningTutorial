cde558fd7ba6e0279789e94bc1967edec05d69db
samiyagmur <77567437+samiyagmur@users.noreply.github.com>
2022-03-11 14:56:12 +0300

    master

---
+++ c#.ders.8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_.ders._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
