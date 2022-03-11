40d91b63f7b8da17421154ed2aa8a564e8ffae47
samiyagmur <77567437+samiyagmur@users.noreply.github.com>
2022-03-11 16:17:47 +0300

    master

--- c#.ders.8/Program.cs
+++ c#.ders.8/Program.cs
@@ -4,28 +4,30 @@ namespace c_.ders._8
 {
     class Program
     {
-        static async void Main(string[] args)
-        {   
+        static void Main(string[] args)
+        {
             //Ekrandan girilen sayıya kadar olan tek sayıları yazdır.
             Console.WriteLine("Lütfen bir sayı giriniz: ");
             int sayac = int.Parse(Console.ReadLine());
-            
-            for (int i = 0 ; i < sayac; i++)
+
+            for (int i = sayac - 1; i >= 0; i--)
             {
-                if(i%2 == 1){
+                if (i % 2 == 1)
+                {
                     Console.WriteLine(i);
                 }
-                
+
             }
+            //1 ile 1000 arasındaki tek ve çif sayıların ayrı ayrı toplamı.
             int tekToplam = 0;
             int ciftToplam = 0;
-            for(int i = 1; i<1000;i++)
+            for (int i = 1; i <= 1000; i++)
             {
-                if (i%2 == 1)
+                if (i % 2 == 1)
                 {
                     tekToplam = i + tekToplam;
                 }
-                else if (i%2 == 0)
+                else if (i % 2 == 0)
                 {
                     ciftToplam = i + ciftToplam;
                 }
@@ -33,6 +35,31 @@ namespace c_.ders._8
 
             Console.WriteLine("Tek sayıların toplamı: " + tekToplam);
             Console.WriteLine("Çift sayıların toplamı: " + ciftToplam);
+
+            //break,(Döngüyü sonlandırmayı sağlar)
+            //continue(Bir sonraki satırı atlayarak sonraki satırların işlemesini sağlar.)
+
+            for( int i = 1;i < 10; i++)
+            {
+                if(i == 4)
+                        break;// i'4e eşit olduğu zaman döngüden çıkacak
+                    Console.WriteLine(i);
+                
+               
+            }
+            for( int i = 1;i < 10; i++)
+            {
+                if(i == 4)
+                        continue;// i'4e eşit olduğu zaman döngüden çıkacak
+                    Console.WriteLine(i);
+                
+               
+            }
+            for( ; ; )//Sonsuz döngü.Kullanıcıdan aldığınız bir sayının sonucunda break komutu ile kırabilirsiniz.
+            {
+                         
+               
+            }
 
         }
     }
