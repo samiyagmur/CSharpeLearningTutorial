f814f678c5365670a4f8dfa72ddb1884be029fe4
samiyagmur <samiyagmur92@gmail.com>
2022-03-14 23:19:49 +0300

    master

---
+++ c#ders.13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c_ders._13
+{
+    class Program
+    {
+        static void Main(string[] args)//out parametre//bir fonksiyonda iş yaptırıp ve onun sonucunda bir değeri setlemek istiyorsanız ve o setlediğini kullanmak istiyorsanız bu out parameyredr
+        {
+            string sayi = "999";
+            int outSayi;
+            bool sonuc = int.TryParse(sayi,out outSayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+                
+            }
+
+
+        }
+    }
+}
