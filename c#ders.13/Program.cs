2bc928fec9850898706cb2d80fa80eb53f8411f7
samiyagmur <samiyagmur92@gmail.com>
2022-03-15 01:00:09 +0300

    commit

--- c#ders.13/Program.cs
+++ c#ders.13/Program.cs
@@ -20,7 +20,44 @@ namespace c_ders._13
                 
             }
 
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+            //Method Aşırı Yükleme--Overloading
 
+            string ifade = "sami yagmur";
+            instance.EkranYazdır(ifade);
+            instance.EkranYazdır("Sami ","yagmur");
+
+            //Metot Imzası
+            //metotAdı + parametre sayisi + parametre tipi
         }
+    }
+    class Metotlar
+    {
+        public void Topla(int a,int b, out int toplam)
+        {
+            toplam = a+b;
+        }
+        public void EkranYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        //Bu iki metodun aynı olması dikkat çekici!
+        //işte bu durumda değişkenin hem int hemde string türünden yazdırabiliriz.
+        //Bu işlem overload oluyor.
+        //Peki hangigisini yazacağına nasıl karar veriyor.
+        //Şu an methodu string le çağırdığımız için string parametreli methodu işletecek.Eğer int olsaydı int parametreli olan işleyecekti
+        //Peki bunu yaparken nasıl bir yöntem uyguluyor.İşte bunda.Method imzası diyoruz.
+        public void EkranYazdır(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranYazdır(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
+        }
+        
     }
 }
