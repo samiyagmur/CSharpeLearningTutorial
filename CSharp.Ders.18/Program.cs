b6d7436cdaa5f868e6f98129f33fb2eb06bf0303
samiyagmur <samiyagmur92@gmail.com>
2022-03-18 01:28:50 +0300

    commit

---
+++ CSharp.Ders.18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharp.Ders._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Koleksiyonlar//Arrayın dezavantaçları aynı veri tipinde olmak zorunda
+            //Arrayın boyudtu önceden belirli olmalı
+            //Bu tarz dezavantajdan koleksiyonlara ihtiyaç duyuluyor
+            //Ne kdar veri tutacağımız belli olmadığı için hızllı erişim sağlıyabilmek için koleksiyon ortaya çıkmış
+            //Arrayın aksine kullanılan bilgisayarın rami yetti kdarı ile veri atanabilir.
+            //Koleksiyonlar farklı veri tipleri içine atabilir
+            //Koleksiyonlar nesnelerden oluluşur.Farklı veri tiplerini içerisini atanabilir.
+            //Nesnenin tipide objecttir.
+            //koleksiyonlar System.colection'la birlikte çalışır
+            //koleksiyonların dezavantajı
+            //Dotnet veri tipleri iki tiptir:değer tipleri,referans tipleri
+            //Değer tipleri belleğin stack bölgesinde tutulurken//Değer tipleri:int,long,float,decimal,char,bool,short,struct
+            //Referans tipleri belleğin heap bölgesinde tutulur//String,object,class,interface,array,pointer
+            //Referans tipindeki bir değişkene bellekte yer ayırırken de “new” anahtar kelimesi kullanılır.
+            //Bir değer tipinin referans tipine dönüşmesine boxing tam tersine unboxing denir.
+            //Bir değer tipini bir koleksiyona atmaya çalıştığımızda değer tipini referans tipine dönüştürmeye ihtiyacımız var.
+            //O koleksiyondan bir şey okumaya çalıştığımızda unboxing yapmış oluyoruz.
+            //boxing ve unboxing maliyetli işlemler
+            //Genel amaçlı koleksiyonlar:List,Arraylist,Dictionary,HashTable,Queue,Stack Sorted List
+            //Özel amaçlı koleksiyonlar:Hybrit Dictionary,Name Value Dictionary,String Collection,String Dictionary
+            //Bit tabanlı koleksiyonlar:Bit Array
+            
+        }
+    }
+}
---
+++ CSharp.Ders.19/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp.Ders._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<t> class
+            //System.Collections.Generic
+            //T-> object türündedir.
+
+            List<int> sayiListesi = new List<int>();
+
+            sayiListesi.Add(23);
+            sayiListesi.Add(10);
+            sayiListesi.Add(4);
+            sayiListesi.Add(5);
+            sayiListesi.Add(92);
+            sayiListesi.Add(34);
+
+            List<string> renkListesi = new List<string>();
+            renkListesi.Add("Kırmızı");
+            renkListesi.Add("Mavi");
+            renkListesi.Add("Turuncu");
+            renkListesi.Add("Sarı");
+            renkListesi.Add("Yeşil");
+            //List metotlar
+
+            //Count
+            Console.WriteLine(renkListesi.Count);
+            Console.WriteLine(sayiListesi.Count);
+
+            //Eleman yazdırma
+            foreach(int sayi in sayiListesi)
+            {
+                Console.WriteLine(sayi);
+            }
+            foreach (string renk in renkListesi)
+            {
+                Console.WriteLine(renk);
+            }
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+            //Listeden eleman çıkarma
+            sayiListesi.Remove(4);
+            renkListesi.Remove("Yeşil");
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+            //Index belirterek ile çıkarma
+            sayiListesi.RemoveAt(0);
+            renkListesi.RemoveAt(1);
+            sayiListesi.ForEach(sayi => Console.WriteLine(sayi));
+            renkListesi.ForEach(renk => Console.WriteLine(renk));
+
+            //Liste içerisinde arama
+            if(sayiListesi.Contains(10))
+                 Console.WriteLine("10 Liste içerisnde bulundu!");
+            //Eleman ile index'e erişme
+            Console.WriteLine(renkListesi.BinarySearch("Sarı"));
+
+            //Diziyi List'e çevirme
+            string[] hayvanlar = { "kedi", "köpek", "kuş" };
+            List<string> hayvanlarListesi = new List<string>(hayvanlar);
+
+            //Listeyi nasıl temizleriz?
+            hayvanlarListesi.Clear();
+
+            //Liste içerisinde nesne tutmak
+            List<Kullanıcılar> kullanıcıListesi = new List<Kullanıcılar>();
+
+            Kullanıcılar kullanıcı1 = new Kullanıcılar();
+            kullanıcı1.Isim = "Ayşe";
+            kullanıcı1.Soyisim = "Yılmaz";
+            kullanıcı1.Yas = 26;
+
+            Kullanıcılar kullanıcı2 = new Kullanıcılar();
+            kullanıcı2.Isim = "Özcan";
+            kullanıcı2.Soyisim = "Çalışkan";
+            kullanıcı2.Yas = 26;
+
+            kullanıcıListesi.Add(kullanıcı1);
+            kullanıcıListesi.Add(kullanıcı2);
+
+            List<Kullanıcılar> yeniListe = new List<Kullanıcılar>();
+
+            yeniListe.Add(new Kullanıcılar()
+            {
+                Isim = "deniz",
+                Soyisim = "Arda",
+                Yas = 24
+            });
+
+            foreach(var kullanıcı in kullanıcıListesi)
+            {
+                Console.WriteLine("Kullanıcı ismi:" + kullanıcı.Isim);
+                Console.WriteLine("Kullanıcı ismi:" + kullanıcı.Soyisim);
+                Console.WriteLine("Kullanıcı ismi:" + kullanıcı.Yas);
+
+            }
+            yeniListe.Clear();
+
+
+        }
+    }
+    public class Kullanıcılar
+    {
+        private string isim;
+
+        private string soyisim;
+
+        private int yas;
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+        public int Yas { get => yas; set => yas = value; }
+    }
+}
