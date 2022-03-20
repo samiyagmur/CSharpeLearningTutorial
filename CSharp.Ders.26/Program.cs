4f0dff26a2d69f86bb29452fa66508a355a1b552
samiyagmur <samiyagmur92@gmail.com>
2022-03-20 19:35:18 +0300

    commit

---
+++ CSharp.Ders.26/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CSharp.Ders._26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Static sınıf
+            //Bır sınıf içerisinde statik olmayan fieldlara metotlara özelliklere o sınıftan oluşturduğunuz nesne aracı ile erişiyoruz.
+            //Statik olan elemanlara o sınıfın adı ile erişilebilir.
+
+            Console.WriteLine("Çalışan Syısı: {0}", Calisan.CalisanSayisi);
+
+            Calisan calisan = new Calisan("Ayşe", "Yılmaz", "IK");
+            Console.WriteLine("Çalışan Syısı: {0}", Calisan.CalisanSayisi);
+            
+            Calisan calisan1 = new Calisan( "Sami", "Yagmur", "IK" );
+            Calisan calisan2 = new Calisan( "Zikriye", "dönmez", "IK" );
+            Console.WriteLine("Çalışan Syısı: {0}", Calisan.CalisanSayisi);
+
+            Console.WriteLine("Toplama işlemi sonucu: {0}", Islemler.Topla(100, 200));
+            Console.WriteLine("Toplama işlemi sonucu: {0}", Islemler.Topla(400, 50));
+
+
+            //Islemler Sınıfın üyelerine erişmeye çalıştığımızda stastic olduğu için bu sınıf fabu şekilde erişim yapılmaz.
+            //Islemler ıslemler = new Islemler();
+
+
+        }
+    }
+    //statik olmayan class lar içerisindeki nesneye özgü iken.
+    //Onun içerisindeki statik olmayan üyseler,
+    //o sınıfn nesnelerine özel set'leniyo ve get'leniyoken,
+    //bir sınıf içerisinde statik olan üyeler sınıf bazında atanır
+    class Calisan//bir sınıfı statik yaparsanız grubun tüm üyeleri static olması gerekir.
+    {               
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi;}
+
+        private string Isım;
+
+        private string Soyisim;
+
+        private string Departman;
+
+        static Calisan() //statik kurucuların erişim belirteçleri yoktur
+        {
+            calisanSayisi = 0;
+        }
+
+        public Calisan(string isim,string soyisim,string departman)//krurucuların erişim belirteçleri geri dönüş tipi yoktur.
+        {
+            this.Isım = isim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+               
+    }
+
+    static class Islemler
+    {
+        public static long Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public static long Cıkar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+
+}
--- c#ders.16/Program.cs
+++ c#ders.16/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace cSharp.ders._16
 {
@@ -8,56 +9,61 @@ namespace cSharp.ders._16
         {
             string degisken = "Dersimiz CSarp, Hoşgeldiniz!";
             string degisken2 = "CSharp";
-
-            Console.WriteLine(degisken.Length);//Degişkenin boyutunu kaç karakter olduğunu ekrana yazar
-
-            //ToUppet,ToLower
-            Console.WriteLine(degisken.ToUpper());//Değişkenin harflerini büyütür
-            Console.WriteLine(degisken.ToLower());//Değişkenin harflerini küçültür
-
-            //Concat
-            Console.WriteLine(string.Concat(degisken, "Merhaba!"));//Merhabayı değişkenin sonuna atarak birleştirir
-
-            //Compare,CompareTo
-            Console.WriteLine(degisken.CompareTo(degisken2));//2 değişken karakter sayılarına göre true yada false döndürür
-            Console.WriteLine(String.Compare(degisken,degisken2,true));//Karşılaştırır fakat büyük küçük harf denetimi de yapar.
-                                                                       //True yaparsak Bunu açmış oluruz,False yaparsak kapatmış oluruz
-            Console.WriteLine(String.Compare(degisken, degisken2, false));
-
-            //Contains//2 değişken içerisinde arama yapar eğer aynı karakterleri bulursa true döner.
-            Console.WriteLine(degisken.Contains(degisken2));
-            Console.WriteLine(degisken.EndsWith("Hoşgeldiniz!"));
-            Console.WriteLine(degisken.StartsWith("Merhaba!"));
-
-            //IndexOf//Beliirttiğimiz ifadenin hangi indexte olduğunu söyler//bulamazsa -1 döner.
-            Console.WriteLine(degisken.IndexOf("CS"));
-            Console.WriteLine(degisken.IndexOf("sami"));
-            Console.WriteLine(degisken.LastIndexOf("i"));//Sondan başlayarak istenilen karakterinin ilkini ekrana yazdırır.
             
-            //Insert//belirtilen indexten başlayarak istediğimiz karakteri ekler.
-            Console.WriteLine(degisken.Insert(0, "Merhaba! "));
+            string[]der = degisken.ToCharArray();
+            
+            Array.Sort(der);
+            string deg = der.ToString();
+            Console.WriteLine(deg);
+            // Console.WriteLine(degisken.Length);//Degişkenin boyutunu kaç karakter olduğunu ekrana yazar
 
-            //PadLeft,PadRight//Degişken 2 sonuna 30 karakteryada kadar boşluk ekler.Değişken 2 nin karakter sayısı 30 dan küçükse hiç bişi yapmaz
-            Console.WriteLine(degisken + degisken2.PadLeft(30));
-            Console.WriteLine(degisken + degisken2.PadLeft(30,'*'));
-            Console.WriteLine(degisken + degisken2.PadRight(50));
-            Console.WriteLine(degisken + degisken2.PadRight(50, '-'));
+            // //ToUppet,ToLower
+            // Console.WriteLine(degisken.ToUpper());//Değişkenin harflerini büyütür
+            // Console.WriteLine(degisken.ToLower());//Değişkenin harflerini küçültür
 
-            //Remove//Belirtilen indexi ve indexler arasını siler
-            Console.WriteLine(degisken.Remove(10));
-            Console.WriteLine(degisken.Remove(5,3));
-            Console.WriteLine(degisken.Remove(0,1));
+            // //Concat
+            // Console.WriteLine(string.Concat(degisken, "Merhaba!"));//Merhabayı değişkenin sonuna atarak birleştirir
 
-            //Replace//Belirtilen 1. parametyi  2. parametreyle değiştir
-            Console.WriteLine(degisken.Replace("CSharp", "C#"));
-            Console.WriteLine(degisken.Replace(" ", "*"));
+            // //Compare,CompareTo
+            // Console.WriteLine(degisken.CompareTo(degisken2));//2 değişken karakter sayılarına göre true yada false döndürür
+            // Console.WriteLine(String.Compare(degisken,degisken2,true));//Karşılaştırır fakat büyük küçük harf denetimi de yapar.
+            //                                                            //True yaparsak Bunu açmış oluruz,False yaparsak kapatmış oluruz
+            // Console.WriteLine(String.Compare(degisken, degisken2, false));
 
-            //Split//Belirttiğimiz karakterden bölerek yeni bir liste oluştur.Eğer sonuna[] belirli bir index yeni listenin o indexini ekrana yazar
-            Console.WriteLine(degisken.Split(' ')[1]);
+            // //Contains//2 değişken içerisinde arama yapar eğer aynı karakterleri bulursa true döner.
+            // Console.WriteLine(degisken.Contains(degisken2));
+            // Console.WriteLine(degisken.EndsWith("Hoşgeldiniz!"));
+            // Console.WriteLine(degisken.StartsWith("Merhaba!"));
 
-            //Substring//Belitilen indexten itibaren ekrana yazar.Bitiş endexinide belirtebilirsizni
-            Console.WriteLine(degisken.Substring(4));
-            Console.WriteLine(degisken.Substring(4,6));
+            // //IndexOf//Beliirttiğimiz ifadenin hangi indexte olduğunu söyler//bulamazsa -1 döner.
+            // Console.WriteLine(degisken.IndexOf("CS"));
+            // Console.WriteLine(degisken.IndexOf("sami"));
+            // Console.WriteLine(degisken.LastIndexOf("i"));//Sondan başlayarak istenilen karakterinin ilkini ekrana yazdırır.
+            
+            // //Insert//belirtilen indexten başlayarak istediğimiz karakteri ekler.
+            // Console.WriteLine(degisken.Insert(0, "Merhaba! "));
+
+            // //PadLeft,PadRight//Degişken 2 sonuna 30 karakteryada kadar boşluk ekler.Değişken 2 nin karakter sayısı 30 dan küçükse hiç bişi yapmaz
+            // Console.WriteLine(degisken + degisken2.PadLeft(30));
+            // Console.WriteLine(degisken + degisken2.PadLeft(30,'*'));
+            // Console.WriteLine(degisken + degisken2.PadRight(50));
+            // Console.WriteLine(degisken + degisken2.PadRight(50, '-'));
+
+            // //Remove//Belirtilen indexi ve indexler arasını siler
+            // Console.WriteLine(degisken.Remove(10));
+            // Console.WriteLine(degisken.Remove(5,3));
+            // Console.WriteLine(degisken.Remove(0,1));
+
+            // //Replace//Belirtilen 1. parametyi  2. parametreyle değiştir
+            // Console.WriteLine(degisken.Replace("CSharp", "C#"));
+            // Console.WriteLine(degisken.Replace(" ", "*"));
+
+            // //Split//Belirttiğimiz karakterden bölerek yeni bir liste oluştur.Eğer sonuna[] belirli bir index yeni listenin o indexini ekrana yazar
+            // Console.WriteLine(degisken.Split(' ')[1]);
+
+            // //Substring//Belitilen indexten itibaren ekrana yazar.Bitiş endexinide belirtebilirsizni
+            // Console.WriteLine(degisken.Substring(4));
+            // Console.WriteLine(degisken.Substring(4,6));
 
 
 
