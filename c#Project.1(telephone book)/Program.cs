78988242f41f309668adcbebaed91299537bccdc
samiyagmur <samiyagmur92@gmail.com>
2022-04-06 09:33:02 +0300

    Parika.Dev.C#.101

--- c#Project.1(telephone book)/Program.cs
+++ c#Project.1(telephone book)/Program.cs
@@ -58,25 +58,5 @@ namespace Telefon.Rehberi.Uygulaması
         }
     }
 
-    public class PhoneBook
-    {
-       
-        private string name;
-        private string surName;
-        private string phoneNum;
         
-        public string Name { get => name; set => name = value.ToUpper(); }
-        public string SurName { get => surName; set => surName = value.ToUpper(); }
-        public string PhoneNum { get => phoneNum; set => phoneNum = value; }
-
-
-        public static List<PhoneBook> Person = new List<PhoneBook>()
-        {
-                new PhoneBook { Name="SAMİ", SurName="YAGMUR" ,PhoneNum= "5384786958"  },
-                new PhoneBook { Name="KEMAL", SurName ="SUNAL" ,PhoneNum= "5384768214"   },
-                new PhoneBook { Name="HALİT", SurName="AKÇATEPE",PhoneNum=  "5428469472" },
-                new PhoneBook { Name="TARIK", SurName="AKAN",PhoneNum=  "5451649721" },
-                new PhoneBook { Name="YILMAZ", SurName="GÜNEY",PhoneNum=  "5068469214" }
-        };
-    }    
 }
